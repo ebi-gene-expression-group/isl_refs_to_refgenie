b7cf091b42edb201d7a7a135f13358e2aff8c2f6
Jonathan Manning <jmanning@ebi.ac.uk>
2021-05-18 15:55:33 +0100

    Add additional tags when building assets

--- bin/build_asset.sh
+++ bin/build_asset.sh
@@ -5,9 +5,14 @@ recipe=$2
 fileType=$3
 filePath=$4
 refgenieDir=$5
-tag=${6:-'default'}
+tags=${6:-'default'}
 
 completedFlag="${refgenieDir}/alias/$assembly/$recipe/$tag/_refgenie_build/refgenie_completed.flag"
+
+# Assemblies or tags with '.' cause refgenie errors
+
+assembly=$(echo -e "$assebly" | sed 's/\./_/g')
+tags=$(echo -e "$tags" | sed 's/\./_/g' | sed 's/,/ /g')
 
 if [ -e "$completedFlag" ]; then
     echo "$completedFlag already present, $recipe build cancelled" 1>&2
@@ -16,16 +21,29 @@ else
     if [ -n "$fileType" ]; then
         filePart="--files $fileType=$filePath "
     fi
-    refgenieCommand="refgenie build $assembly/${recipe}:${tag} ${filePart}-c ${refgenieDir}/genome_config.yaml -R"
-    echo $refgenieCommand
-    eval $refgenieCommand
-   
-    if [ -e "$completedFlag" ]; then
-        echo "Refgenie build process successful"
-    else
-        echo "Refgenie build process failed, '$completedFlag' not present" 1>&2
-        exit 1
-    fi
+
+    built=0
+    firsttag=
+
+    for tag in $tags; do
+        if [ "$built" -eq 0 ]; then
+            firsttag=$tag        
+            refgenieCommand="refgenie build $assembly/${recipe}:${firsttag} ${filePart}-c ${refgenieDir}/genome_config.yaml -R"
+            echo $refgenieCommand
+            eval $refgenieCommand
+           
+            if [ -e "$completedFlag" ]; then
+                echo "Refgenie build process successful"
+            else
+                echo "Refgenie build process failed, '$completedFlag' not present" 1>&2
+                exit 1
+            fi
+
+        else
+            refgenie tag $assembly/${recipe}:${firsttag}--tag $tag
+        fi
+    done
+
 fi
 
 touch .done
