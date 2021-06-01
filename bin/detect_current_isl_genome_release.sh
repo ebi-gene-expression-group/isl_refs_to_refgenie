f591628b20c9341e1f3bebdf35da3edd721e3e28
Jonathan Manning <jmanning@ebi.ac.uk>
2021-06-01 11:28:38 +0100

    Fix release detction

--- bin/detect_current_isl_genome_release.sh
+++ bin/detect_current_isl_genome_release.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 configFile=$1
-
 config=$(cat $configFile)
 
 echo -e "$config" | grep "WBPS" > /dev/null
 isWBPS=$?
 
 if [ "$isWBPS" -eq '0' ]; then
-    version=$(echo -e "$config" | grep -Eo -m 1 "WBPS[0-9]+" | grep -o "[0-9]+")
+    version=$(echo -e "$config" | grep -Eo -m 1 "WBPS[0-9]+" | grep -Eo "[0-9]+")
 else 
     gtf_file=$(echo -e "$config" | grep gtf_file | awk -F '=' '{print $2}')
     echo -e "$gtf_file" | grep "\.gz" > /dev/null
@@ -17,6 +16,11 @@ else
     else
         version=$(echo -e "$gtf_file" | awk -F'.' '{print $(NF-1)}')
     fi
+fi
+
+if [ -z "$version" ]; then
+    echo "Failed to detect current version" 1>&2
+    exit 1
 fi
 
 echo -n $version
--- bin/detect_newest_isl_genome_release.sh
+++ bin/detect_newest_isl_genome_release.sh
@@ -4,13 +4,18 @@ species=$1
 data_dir=$2
 gtf_pattern=$3
 
-find_file_pattern=$(basename "$gtf_pattern" | sed 's/RELNO/*/')
+find_file_pattern=$(basename "$gtf_pattern" | sed 's/RELNO/?*/')
 extract_release_pattern=$(basename "$gtf_pattern" | sed 's/RELNO/\(\.\*)/')
 
 newest_file=$(ls $data_dir/reference/$species/$find_file_pattern | sort -rV | head -n 1)
 
 if [[ "$(basename $newest_file)" =~ $extract_release_pattern ]]; then
     release=${BASH_REMATCH[1]}
+fi
+
+if [ -z "$release" ]; then
+    echo "Failed to detect newest release by looking for latest $data_dir/reference/$species/$find_file_pattern" 1>&2
+    exit 1
 fi
 
 echo -n "$release"
