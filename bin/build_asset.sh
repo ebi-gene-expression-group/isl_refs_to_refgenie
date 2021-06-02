f73c4932e1c948857ce33e730c26ad3f32780231
Jonathan Manning <jmanning@ebi.ac.uk>
2021-06-02 12:53:21 +0100

    Can't tag assets in same dir, so just remake for multiple tags

--- bin/build_asset.sh
+++ bin/build_asset.sh
@@ -6,6 +6,8 @@ fileType=$3
 filePath=$4
 refgenieDir=$5
 tags=${6:-'default'}
+tagPrefix=${7:-''}
+assets=${8:-''}
 
 # Assemblies or tags with '.' cause refgenie errors
 
@@ -13,21 +15,26 @@ if [ -e "$completedFlag" ]; then
     echo "$completedFlag already present, $recipe build cancelled" 1>&2
 else
     filePart=''
+    assetsPart=''
     if [ -n "$fileType" ]; then
         filePart="--files $fileType=$filePath "
+    elif [ -n "$assets" ]; then
+        assetsPart="--assets $assets "
     fi
 
     built=0
     firsttag=
 
     for tag in $(echo "$tags" | tr -d '\n' | sed 's/,/ /g'); do
-        completedFlag="${refgenieDir}/alias/$assembly/$recipe/$tag/_refgenie_build/refgenie_completed.flag"
+        completedFlag="${refgenieDir}/alias/$assembly/$recipe/${tagPrefix}$tag/_refgenie_build/refgenie_completed.flag"
 
         if [ "$built" -eq 0 ]; then
             firsttag=$tag        
-            refgenieCommand="refgenie build $assembly/${recipe}:${firsttag} ${filePart}-c ${refgenieDir}/genome_config.yaml"
+            refgenieCommand="refgenie build $assembly/${recipe}:${tagPrefix}${firsttag} ${filePart}${assetsPart}-c ${refgenieDir}/genome_config.yaml"
         else
-            refgenieCommand="refgenie tag $assembly/${recipe}:${firsttag} --tag $tag"
+            # See https://github.com/refgenie/refgenie/issues/252
+            #refgenieCommand="refgenie tag $assembly/${recipe}:${firsttag} --tag $tag -c ${refgenieDir}/genome_config.yaml"
+            refgenieCommand="refgenie build $assembly/${recipe}:${tagPrefix}${tag} ${filePart}${assetsPart}-c ${refgenieDir}/genome_config.yaml"
         fi            
 
         echo $refgenieCommand
@@ -35,6 +42,7 @@ else
        
         if [ -e "$completedFlag" ]; then
             echo "Refgenie build process successful"
+            built=1
         else
             echo "Refgenie build process failed, '$completedFlag' not present" 1>&2
             exit 1
