00dca68be2d26b334cace07a4a9f8c37dd2f0314
Jonathan Manning <jmanning@ebi.ac.uk>
2021-10-06 14:37:25 +0100

    Allow that assembly may have been set to blank value in config files

--- bin/detect_current_isl_genome_assembly.sh
+++ bin/detect_current_isl_genome_assembly.sh
@@ -8,10 +8,15 @@ config=$(cat $configFile)
 # In theory, the species-wise config file might be using a different reference.
 # Check for explicit references, assume same as ISL config otherwise.
 
+assembly=''
 grep "#assembly" $configFile > /dev/null
 if [ $? -eq 0 ]; then
-    echo -e "$config" | grep '#assembly' | awk -F'=' '{print $2}' | tr -d '\n'
-else
+    assembly=$(echo -e "$config" | grep '#assembly' | awk -F'=' '{print $2}' | tr -d '\n')
+fi
+
+if [ -z "$assembly" ]; then
     species=$(echo -e "$config" | grep "species=" | awk -F'=' '{print $2}' | tr -d '\n')
-    grep "^$species " $islGenomesFile | awk '{print $7}' | tr -d '\n'
-fi 
+    assembly=$(grep "^$species " $islGenomesFile | awk '{print $7}' | tr -d '\n')
+fi
+
+echo -n "$assembly" 
